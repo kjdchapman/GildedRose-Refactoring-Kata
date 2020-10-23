8b1d6f6d636828caeb8e5e095a1f82f710385804
Kyle <kjdchapman@gmail.com>
2020-10-23 14:33:17 +0100

    Add test for sell in values going negative

--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -20,6 +20,13 @@ describe GildedRose do
 
         expect(items[0].quality).to be_zero
       end
+
+      it "continues to reduce sell in below zero" do
+        items = [Item.new("foo", sell_in, quality)]
+        GildedRose.new(items).update_quality()
+
+        expect(items[0].sell_in).to eq -1
+      end
     end
 
     context do
