22cf43f03d841742c97bf0a68595e003a4f23e78
Kyle <kjdchapman@gmail.com>
2020-10-23 16:21:42 +0100

    Use more meaningful test descriptions

--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -21,48 +21,34 @@ describe GildedRose do
 
       expect(items[0].quality).to eq minimum_quality
     end
+
+    it "reduces sell_in by 1" do
+      _ = ""
+      __ = 0
+      sell_in = 10
+
+      items = [Item.new(_, sell_in, __)]
+      GildedRose.new(items).update_quality()
+
+      expect(items[0].sell_in).to eq sell_in - 1
+    end
+
+    it "continues to reduce sell in below zero" do
+      _ = ""
+      __ = 0
+      past_sell_by_date = 0
+
+      items = [Item.new(_, past_sell_by_date, __)]
+      GildedRose.new(items).update_quality()
+
+      expect(items[0].sell_in).to eq -1
+    end
   end
 
   describe 'an item without a special name' do
-    context 'with quality 0 and sell in 0' do
-      let(:sell_in) { 0 }
-      let(:quality) { 0 }
-
-      it "continues to reduce sell in below zero" do
-        items = [Item.new("foo", sell_in, quality)]
-        GildedRose.new(items).update_quality()
-
-        expect(items[0].sell_in).to eq -1
-      end
-    end
-
-    context "with quality 10 and sell in 0" do
-      let(:sell_in) { 0 }
-      let(:quality) { 10 }
-
-      it "reduces quality to 8" do
-        items = [Item.new("foo", sell_in, quality)]
-        GildedRose.new(items).update_quality()
-
-        expect(items[0].quality).to eq 8
-      end
-    end
-
-    context "with quality 10 and sell in 1" do
+    context "when it has sell in days left" do
       let(:sell_in) { 1 }
       let(:quality) { 10 }
-
-      it "reduces quality to 9" do
-        items = [Item.new("foo", sell_in, quality)]
-        GildedRose.new(items).update_quality()
-
-        expect(items[0].quality).to eq 9
-      end
-    end
-
-    context "with quality 1 and sell in 1" do
-      let(:sell_in) { 1 }
-      let(:quality) { 1 }
 
       it "reduces quality by 1" do
         items = [Item.new("foo", sell_in, quality)]
@@ -70,38 +56,43 @@ describe GildedRose do
 
         expect(items[0].quality).to eq quality - 1
       end
+    end
 
-      it "reduces sell_in by 1" do
+    context "when passed sell by date" do
+      let(:sell_in) { 0 }
+      let(:quality) { 10 }
+
+      it "reduces quality by 2" do
         items = [Item.new("foo", sell_in, quality)]
         GildedRose.new(items).update_quality()
 
-        expect(items[0].sell_in).to eq sell_in - 1
+        expect(items[0].quality).to eq quality - 2
       end
     end
   end
 
   describe 'an item named "aged brie"' do
-    context "with quality 10 and sell in 1" do
+    context "when it has sell in days left" do
       let(:sell_in) { 1 }
       let(:quality) { 10 }
 
-      it "increases quality to 11" do
+      it "increases quality by 1" do
         items = [Item.new("Aged Brie", sell_in, quality)]
         GildedRose.new(items).update_quality()
 
-        expect(items[0].quality).to eq 11
+        expect(items[0].quality).to eq quality + 1
       end
     end
 
-    context "with quality 10 and sell in 0" do
+    context "when passed sell by date" do
       let(:sell_in) { 0 }
       let(:quality) { 10 }
 
-      it "increases quality to 12" do
+      it "increases quality by 2" do
         items = [Item.new("Aged Brie", sell_in, quality)]
         GildedRose.new(items).update_quality()
 
-        expect(items[0].quality).to eq 12
+        expect(items[0].quality).to eq quality + 2
       end
     end
   end
