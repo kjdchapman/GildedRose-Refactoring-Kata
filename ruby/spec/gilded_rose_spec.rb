3f30795c3458424698fc8b0090942da7ca95d9b9
Kyle <kjdchapman@gmail.com>
2020-10-23 14:17:40 +0100

    Add test for reducing quality by 1

--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -3,10 +3,28 @@ require_relative '../gilded_rose'
 describe GildedRose do
 
   describe "#update_quality" do
-    it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "fixme"
+    context do
+      let(:sell_in) { 0 }
+      let(:quality) { 0 }
+
+      it "does not change the name" do
+        items = [Item.new("foo", sell_in, quality)]
+        GildedRose.new(items).update_quality()
+
+        expect(items[0].name).to eq "foo"
+      end
+    end
+
+    context do
+      let(:sell_in) { 1 }
+      let(:quality) { 1 }
+
+      it "reduces quality by 1" do
+        items = [Item.new("foo", sell_in, quality)]
+        GildedRose.new(items).update_quality()
+
+        expect(items[0].quality).to eq quality - 1
+      end
     end
   end
 
