a834358a94c6af27dcc60e09d3955917bfb69469
Kyle <kjdchapman@gmail.com>
2020-11-06 15:51:37 +0000

    Let rubocop autofix some things

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -1,52 +1,44 @@
 class GildedRose
-
   def initialize(items)
     @items = items
   end
 
-  def update_quality()
+  # we're updating two things here - quality and sell in
+  # quality usually decreases
+  # quality changes in different ways based on different names
+  def update_quality
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
+      if (item.name != 'Aged Brie') && (item.name != 'Backstage passes to a TAFKAL80ETC concert')
         if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
+          item.quality = item.quality - 1 if item.name != 'Sulfuras, Hand of Ragnaros'
         end
       else
         if item.quality < 50
           item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
+          if item.name == 'Backstage passes to a TAFKAL80ETC concert'
             if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
+              item.quality = item.quality + 1 if item.quality < 50
             end
             if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
+              item.quality = item.quality + 1 if item.quality < 50
             end
           end
         end
       end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
+
+      item.sell_in = item.sell_in - 1 if item.name != 'Sulfuras, Hand of Ragnaros'
+
       if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
+        if item.name != 'Aged Brie'
+          if item.name != 'Backstage passes to a TAFKAL80ETC concert'
             if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
+              item.quality = item.quality - 1 if item.name != 'Sulfuras, Hand of Ragnaros'
             end
           else
             item.quality = item.quality - item.quality
           end
         else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
+          item.quality = item.quality + 1 if item.quality < 50
         end
       end
     end
