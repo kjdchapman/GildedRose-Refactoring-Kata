85aea1f70644a9762071b38c0f0bd9d2be0a41c7
Kyle <kjdchapman@gmail.com>
2021-01-15 15:29:39 +0000

    Express quality logic for normal items in one place
    ('Normal' in this case being 'It doesn't have one of the fancy
    item names)

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -8,16 +8,6 @@ class GildedRose
   # quality changes in different ways based on different names
   def update_quality
     @items.each do |item|
-      if item.name != 'Aged Brie'
-        if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-          if item.name != 'Sulfuras, Hand of Ragnaros'
-            if item.quality > 0
-              item.quality = item.quality - 1
-            end
-          end
-        end
-      end
-
       if item.name == 'Aged Brie'
         if item.quality < 50
           item.quality = item.quality + 1
@@ -50,18 +40,6 @@ class GildedRose
         item.sell_in = item.sell_in - 1
       end
 
-      if item.name != 'Aged Brie'
-        if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-          if item.name != 'Sulfuras, Hand of Ragnaros'
-            if item.sell_in < 0
-              if item.quality > 0
-                item.quality = item.quality - 1
-              end
-            end
-          end
-        end
-      end
-
       if item.name == 'Backstage passes to a TAFKAL80ETC concert'
         if item.sell_in < 0
           item.quality = item.quality - item.quality
@@ -72,6 +50,20 @@ class GildedRose
         if item.sell_in < 0
           if item.quality < 50
             item.quality = item.quality + 1
+          end
+        end
+      end
+
+      if item.name != 'Aged Brie'
+        if item.name != 'Backstage passes to a TAFKAL80ETC concert'
+          if item.name != 'Sulfuras, Hand of Ragnaros'
+            if item.quality > 0
+              item.quality = item.quality - 1
+
+              if item.sell_in < 0
+                item.quality = item.quality - 1
+              end
+            end
           end
         end
       end
