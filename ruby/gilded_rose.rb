cfa26ed4d079340d11034d7a7d71c697720cd0e6
Kyle <kjdchapman@gmail.com>
2021-01-15 14:47:36 +0000

    Simplify negative conditional

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -16,13 +16,13 @@ class GildedRose
         end
       end
 
-      if !((item.name != 'Aged Brie') && (item.name != 'Backstage passes to a TAFKAL80ETC concert'))
+      if (item.name == 'Aged Brie') || (item.name == 'Backstage passes to a TAFKAL80ETC concert')
         if item.quality < 50
           item.quality = item.quality + 1
         end
       end
 
-      if !((item.name != 'Aged Brie') && (item.name != 'Backstage passes to a TAFKAL80ETC concert'))
+      if (item.name == 'Aged Brie') || (item.name == 'Backstage passes to a TAFKAL80ETC concert')
         if item.quality < 50
           if item.name == 'Backstage passes to a TAFKAL80ETC concert'
             if item.sell_in < 11
@@ -32,7 +32,7 @@ class GildedRose
         end
       end
 
-      if !((item.name != 'Aged Brie') && (item.name != 'Backstage passes to a TAFKAL80ETC concert'))
+      if (item.name == 'Aged Brie') || (item.name == 'Backstage passes to a TAFKAL80ETC concert')
         if item.quality < 50
           if item.name == 'Backstage passes to a TAFKAL80ETC concert'
             if item.sell_in < 6
