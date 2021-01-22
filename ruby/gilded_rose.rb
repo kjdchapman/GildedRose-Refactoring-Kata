915064d1f6ce5a59526c40e3fc9d9b29e9027433
Kyle <kjdchapman@gmail.com>
2021-01-22 14:56:40 +0000

    Put 'ifs' with same conditions together

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -18,9 +18,29 @@ class GildedRose
         end
       end
 
+      if item.name != 'Aged Brie'
+        if item.name != 'Backstage passes to a TAFKAL80ETC concert'
+          if item.name != 'Sulfuras, Hand of Ragnaros'
+            if item.sell_in < 1
+              if item.quality > 0
+                item.quality = item.quality - 1
+              end
+            end
+          end
+        end
+      end
+
       if item.name == 'Aged Brie'
         if item.quality < 50
           item.quality = item.quality + 1
+        end
+      end
+
+      if item.name == 'Aged Brie'
+        if item.sell_in < 1
+          if item.quality < 50
+            item.quality = item.quality + 1
+          end
         end
       end
 
@@ -46,29 +66,9 @@ class GildedRose
         end
       end
 
-      if item.name != 'Aged Brie'
-        if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-          if item.name != 'Sulfuras, Hand of Ragnaros'
-            if item.sell_in < 1
-              if item.quality > 0
-                item.quality = item.quality - 1
-              end
-            end
-          end
-        end
-      end
-
       if item.name == 'Backstage passes to a TAFKAL80ETC concert'
         if item.sell_in < 1
           item.quality = item.quality - item.quality
-        end
-      end
-
-      if item.name == 'Aged Brie'
-        if item.sell_in < 1
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
         end
       end
 
