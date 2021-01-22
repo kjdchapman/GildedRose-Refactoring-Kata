3fe5a2cc5c1502e6ac91d4a9cea0db564f5e8a29
Kyle <kjdchapman@gmail.com>
2021-01-22 14:22:02 +0000

    Drag 'sell-in' to top level in each conditional

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -30,30 +30,30 @@ class GildedRose
         end
       end
 
-      if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-        if item.quality < 50
-          if item.sell_in < 11
+      if item.sell_in < 11
+        if item.name == 'Backstage passes to a TAFKAL80ETC concert'
+          if item.quality < 50
             item.quality = item.quality + 1
           end
         end
       end
 
-      if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-        if item.quality < 50
-          if item.sell_in < 6
+      if item.sell_in < 6
+        if item.name == 'Backstage passes to a TAFKAL80ETC concert'
+          if item.quality < 50
             item.quality = item.quality + 1
           end
         end
       end
 
       if item.name != 'Sulfuras, Hand of Ragnaros'
-        item.sell_in = item.sell_in - 1
+        item.sell_in -= 1
       end
 
-      if item.name != 'Aged Brie'
-        if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-          if item.name != 'Sulfuras, Hand of Ragnaros'
-            if item.sell_in < 0
+      if item.sell_in < 0
+        if item.name != 'Aged Brie'
+          if item.name != 'Backstage passes to a TAFKAL80ETC concert'
+            if item.name != 'Sulfuras, Hand of Ragnaros'
               if item.quality > 0
                 item.quality = item.quality - 1
               end
@@ -62,14 +62,14 @@ class GildedRose
         end
       end
 
-      if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-        if item.sell_in < 0
+      if item.sell_in < 0
+        if item.name == 'Backstage passes to a TAFKAL80ETC concert'
           item.quality = item.quality - item.quality
         end
       end
 
-      if item.name == 'Aged Brie'
-        if item.sell_in < 0
+      if item.sell_in < 0
+        if item.name == 'Aged Brie'
           if item.quality < 50
             item.quality = item.quality + 1
           end
