099188da2b72c0509dd12adb13fb338c37d72af6
Kyle <kjdchapman@gmail.com>
2021-01-22 14:50:57 +0000

    Drag 'name' to top level... again
    It might seem like this is a repeat of 0e37852, but doing 3fe5a2c
    (dragging sell-in to the top) helped us make another change first,
    so it does make sense, I promise.

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -30,26 +30,26 @@ class GildedRose
         end
       end
 
-      if item.sell_in < 11
-        if item.name == 'Backstage passes to a TAFKAL80ETC concert'
+      if item.name == 'Backstage passes to a TAFKAL80ETC concert'
+        if item.sell_in < 11
           if item.quality < 50
             item.quality = item.quality + 1
           end
         end
       end
 
-      if item.sell_in < 6
-        if item.name == 'Backstage passes to a TAFKAL80ETC concert'
+      if item.name == 'Backstage passes to a TAFKAL80ETC concert'
+        if item.sell_in < 6
           if item.quality < 50
             item.quality = item.quality + 1
           end
         end
       end
 
-      if item.sell_in < 1
-        if item.name != 'Aged Brie'
-          if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-            if item.name != 'Sulfuras, Hand of Ragnaros'
+      if item.name != 'Aged Brie'
+        if item.name != 'Backstage passes to a TAFKAL80ETC concert'
+          if item.name != 'Sulfuras, Hand of Ragnaros'
+            if item.sell_in < 1
               if item.quality > 0
                 item.quality = item.quality - 1
               end
@@ -58,14 +58,14 @@ class GildedRose
         end
       end
 
-      if item.sell_in < 1
-        if item.name == 'Backstage passes to a TAFKAL80ETC concert'
+      if item.name == 'Backstage passes to a TAFKAL80ETC concert'
+        if item.sell_in < 1
           item.quality = item.quality - item.quality
         end
       end
 
-      if item.sell_in < 1
-        if item.name == 'Aged Brie'
+      if item.name == 'Aged Brie'
+        if item.sell_in < 1
           if item.quality < 50
             item.quality = item.quality + 1
           end
